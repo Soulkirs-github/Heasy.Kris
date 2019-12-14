c0cfd39a6ccec115dd6abdda27dda4301bf7a5fd
Soulkirs-github <52566403+Soulkirs-github@users.noreply.github.com>
2019-12-14 16:44:15 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ exports.PREFIX = '*';
 
 exports.GOOGLE_API_KEY = 'AIzaSyBpV8l4IaeGWYT8CFaRLl5-p_Q8IwE0cKI';
 
-acces_token = process.env
-exports.TOKEN = 'acces_token'
+acces_token = process.env[BOT_TOKEN]
+exports.TOKEN = 'BOT_TOKEN'
 
 exports.OWNERS = '490878001714692117'
